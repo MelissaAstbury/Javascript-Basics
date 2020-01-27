c47c20946c3cd80ecbe81a649faf894420f59596
Melissa Astbury <melissajastbury@gmail.com>
2020-01-27 21:01:01 +0000

    set variables - with prompts - modified HMTL via JS

---
+++ calc.js
@@ -0,0 +1,7 @@
+alert("CONNECTED");
+var age = prompt("What is your age?");
+var days = age * 365;
+alert(age + " years is roughly " + days + " days");
+
+//var daysLeft = 3;
+//document.getElementById("membershipInDays").innerHTML = daysLeft;
